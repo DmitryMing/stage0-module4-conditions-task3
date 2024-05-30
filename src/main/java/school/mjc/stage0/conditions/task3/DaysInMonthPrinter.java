c27ce0df50dcd7446a536aecc66872f3e1b43453
dmitrii <perfilev.dmitrii2020@gmail.com>
2024-05-30 18:51:50 +0800

    1fe

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,5 +2,29 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        // Check if the month number is valid
+        if (month >= 1 && month <= 12) {
+            // Determine the number of days in the month
+            int days;
+            switch (month) {
+                case 2:
+                    days = 28;  // Assuming non-leap year
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    days = 30;
+                    break;
+                default:
+                    days = 31;
+                    break;
+            }
+            // Print the number of days
+            System.out.println(days);
+        } else {
+            // Invalid month number
+            System.out.println("wrong number!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        double res;
+        if (salary <= 0) {
+            System.out.println("wrong input!");
+            return;
+        } else if (salary <= 10000) {
+            res = salary * (1 - 0.15);
+        } else if (salary == 15000) {
+            res = 12300.0;
+        } else {
+            res = salary * (1 - 0.20);
+        }
+        System.out.println(res);
     }
 }
